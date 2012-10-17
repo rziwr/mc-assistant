2eae9c384fe9d12dcc287b3bcaeddec349888197
Igor <zaqwes8811@github.com>
2012-10-17 22:05:21 +0400

    push

---
+++ Modeling/CurrentThresholdCalc.py
@@ -0,0 +1,112 @@
+#-*- coding: utf-8 -*-
+import simpleDataTypesConvertors.Float32Convertors as f32cnv
+import simpleDataTypesConvertors.IntTypeConvertors as tc
+import math
+import ModelADDAC as adda
+from pyDbg.doColoredConsole import co
+
+# Mults
+R1 = 5.11
+R2 = 10.0
+Splitter = R2/(R1+R2)
+
+# Параметры ADDAC
+dVmax = 430.0	# mV сдвиг ЦАП
+VmaxIdeal = 5000.0
+Vmax = VmaxIdeal-dVmax 	# mV - это максимальное значение ЦАП - площадка при выс. сигн.
+capacity = 12
+
+# Параметры входной кривой
+Kiu = 188.0		# mV/A
+dU = 500.0		# mV
+
+# Deprecated
+Resol = math.pow(2, capacity)
+toDigital = Resol/Vmax#/VmaxIdeal
+toAnalog = 1/toDigital
+
+
+''' Ток в код и обратно 
+I, A
+'''
+''' код не переведен в цифру - предст. с плав. точкой '''
+def toDigitalFull( U, mult, toDigital ):
+	Uadc = U * mult	# На плече делителя нужный нам потенц
+	# ADC
+	Udig = toDigital * Uadc
+	return Udig
+	
+def msgSplit(msg):
+	result = ''
+	for at in msg:
+		result += "'"
+		result += at
+		result += "',"
+	return result
+def calcCoeffTransf(I):
+	# Исходная зашумленная зависимость
+	#multer = Splitter	# с делителя на АЦП
+	multer = 1	# если напрямую с датчик Холла
+	U = I * Kiu + dU
+	co.printW( 'Udac : ' + str( U )+'\n')
+	Udig_f = toDigitalFull( U, multer, toDigital )
+	Udig = int( Udig_f )
+	
+	# Рассчитываем шум - смещение по Y
+	Udig_noise_f = toDigitalFull( dU, multer, toDigital )
+	
+	# Очищенное значение - без шума
+	Udig_corr = int(Udig_f - Udig_noise_f)	# суммируются перед оцифровкой
+
+	# коэффициент перевода. Это чистое значение тока - для рассчетов и отображения
+	# Warning : немного расходится с прошитым, но прошитый откалиброван, поэтому 
+	#   наверное пусть как есть
+	Ktrans = I/Udig_corr  # A/ue
+	
+	# переводим в плавающую точку
+	print 'capacity : ' + str( capacity )
+	co.printN( 'Udig_src, ue : ' )
+	co.printE( tc.byte4strhex( Udig )+'\n')
+	
+	msg = "Tr. I,A="
+	msg = msgSplit(msg)
+	f = open('treshes.log', 'at');
+	ILow = I%10
+	IHigh = (I-ILow)/10
+	f.write("\t#define I_MSG_HIGH '"+str(IHigh)+"'\n")
+	f.write("\t#define I_MSG_LOW '"+str(ILow)+"'\n")
+	#f.write("; "+msg+"I_MSG_HIGH,I_MSG_LOW"+",'/'\n")
+	f.write( '\t#define CURRENT_THRESHOLD '+tc.byte4strhex( Udig )+"\t;"+str(I)+" A"+"\n\n")
+	f.close()
+	#print 'Udig_cor, ue :  ' + tc.byte4strhex( Udig_corr )
+	return Udig
+
+''' Просто заглушка '''
+def printRpt( value, valueDisplacemented, valueScaled, valueCode, Kda ):
+	print '\n<< Output values:'
+	print 'Code : '+str(valueCode)
+	print 'Kda : '+str(Kda)
+
+# Run 
+if __name__ == '__main__':
+	# расчет коэффициентов трансформации
+	listOfCurrents = [5, 11, 15, 16, 18, 20]
+	for current in listOfCurrents :
+		msg = '\nI,A : ' + str( current ) + '\n'
+		co.printW( msg )
+		print calcCoeffTransf( current ) 
+	
+	# проверяем
+	'''valueDict = {}
+	valueDict[ 'value' ] = I
+	valueDict['displacement'] = dU
+	valueDict['converter' ] = Kiu
+	valueDict['scale'] = Splitter
+	valueDict['capacity'] = capacity
+	valueDict['Vmax'] = Vmax 
+	code, Kda = adda.modelADC( valueDict, printRpt, adda.calcZeroDisplacmentY )'''
+
+
+
+
+
