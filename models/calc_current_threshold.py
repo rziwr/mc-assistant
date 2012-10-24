2c70d522c46092f79ae893a5ee0a79cbd02e2973
unknown <my@ex.com>
2012-10-24 12:24:09 +0400

    step

--- models/calc_current_threshold.py
+++ models/calc_current_threshold.py
@@ -5,78 +5,68 @@ from pyDbg.doColoredConsole import co
 nprint = co.printN
 wprint = co.printW
 eprint = co.printE
+def wprintValue( name, value ):
+	wprint( name+' : '+str(value)+'\n')
+def nprintValue( name, value ):
+	nprint( name+' : '+str(value)+'\n')
 	
 import simpleDataTypesConvertors.Float32Convertors as f32cnv
 import simpleDataTypesConvertors.IntTypeConvertors as tc
 
 # Читаем конфигурация сенсора
-import math
 import json
 
 # Общая читалка
 from sensors_uni import *
 
-# читае общую конфигурацию
-uni_sensor_sets = get_sensors_cfg()
-
-# читаем конфигурацию для тока
-sets[ 'name' ] = uni_sensor_sets['I']
-sensorSettings = iow.file2list( sets )
-
-# here we are converting python object to json string
-sensor_sets = json.loads( ' '.join(sensorSettings))
-print json.dumps(sensor_sets, sort_keys=True, indent=2)
+# читае конфигурация сенсора
+sensor_sets = get_sensor_cfg('I')
 
 # Настройки прочитаны, можно разбирать их
-
+metroChannal = SensorChannal( sensor_sets,'adc_metro','splitter_metro_parems', value2voltage )
 
 ''' 
 	Ток в код и обратно I, A 
 	код не переведен в цифру - предст. с плав. точкой 
 '''
-def toDigitalFull( U, mult, toDigital ):
-	Uadc = U * mult	# На плече делителя нужный нам потенц
-	# ADC
-	Udig = toDigital * Uadc
-	return Udig
-	
-def wprintValue( name, value ):
-	wprint( name+' : '+str(value)+'\n')
-def nprintValue( name, value ):
-	nprint( name+' : '+str(value)+'\n')
-
+# Example:
 # Uo = R16*Uerr/(R16+R10) = 10*500/(10+5.11) = 330.907 mV
 # 2^10 - 5000 mV
 # x - Uo ; x = 67.76 ue = 68 ue = 0x44 ue
-def calcCoeffTransf(I):
-	# Исходная зашумленная зависимость
-	multer = splitter	# если напрямую с датчик Холла
+def calcCoeffTransf( value, channal, plot_cb ):
+	# Получаем описание канала и кривой сенсора
+	multer = channal.getSplitter()
+	toDigital = channal.toBitCoeff()
 	
-	Udig_f = toDigitalFull( U, multer, toDigital )
-	Udig = int( Udig_f )
+	# Обработка
+	U = channal.sensor_curve( I )
+	
+	# Умножаем на коэфф. перед. аналоговой цепи и "оцифровываем"
+	Uadc = U * multer * toDigital	
+	Udig = int( Uadc )
 	
 	# переводим в плавающую точку
 	nprintValue( 'Udac, mV', U )
-	nprintValue( 'Udig_f, mV',Udig_f)
 	nprintValue( 'Udig, ue', Udig )
-	eprint( '\t'+tc.byte4strhex( Udig )+'\n' )
+	eprint( 'Code WORD: '+tc.byte4strhex( Udig )+'\n' )
 	return Udig
+
 
 # Run 
 if __name__ == '__main__':
 	# расчет коэффициентов трансформации
-	listOfCurrents = [16]
+	listOfCurrents = [0]
 	for I in listOfCurrents :
 		msg = '\nI,A : ' + str( I ) + '\n'
 		wprint( msg )
-		Udig = calcCoeffTransf( I ) 
+		Udig = calcCoeffTransf( I, metroChannal, None ) 
 		
 		# Записать в файл шаблон
 		sets = { 'name': 'threshes.h', 'howOpen': 'w', 'coding': 'cp1251'}
 		lstForWrite = list('')
 		lstForWrite.append('\t#define CURRENT_THRESHOLD '+tc.byte4strhex( Udig )+"\t;"+str(I)+" A"+"\n")
 		iow.list2file( sets=sets, lst=lstForWrite )
-		
+	'''	
 	# смещение нуля при обратоной обработке
 	I = 0
 	Udig = calcCoeffTransf( I ) 
@@ -86,10 +76,9 @@ if __name__ == '__main__':
 	lstForWrite.append('#ifdef HALL_SENSOR')
 	lstForWrite.append('\t#define ZERO_HALL_CORRECT '+tc.byte4strhex( Udig )+"\t;"+str(I)+" A"+"")
 	lstForWrite.append('#endif ;HALL_SENSOR')
-	iow.list2file( sets=sets, lst=lstForWrite )
+	iow.list2file( sets=sets, lst=lstForWrite )'''
 
 ''' 
-
 # коэффициент перевода. Это чистое значение тока - для рассчетов и отображения
 # Warning : немного расходится с прошитым, но прошитый откалиброван, поэтому 
 #   наверное пусть как есть
--- models/sensors_uni.py
+++ models/sensors_uni.py
@@ -1,6 +1,7 @@
 #-*- coding: utf-8 -*-
 #from sensors_uni import *
 import json
+import math
 
 import usaio.io_wrapper as iow
 
@@ -12,35 +13,66 @@ def get_sensors_cfg():
 	print json.dumps(sensor_sets, sort_keys=True, indent=2)
 	return sensor_sets
 	
-class SensorChannal():
-	def __init__(self, sets, addacName, hxName):
+def get_sensor_cfg( name ):
+	# читае общую конфигурацию
+	uni_sensor_sets = get_sensors_cfg()
+	sets = { 'name': 'sensors_cfg_names.json', 'howOpen': 'r', 'coding': 'cp1251'}
+	# читаем конфигурацию для тока
+	sets[ 'name' ] = uni_sensor_sets[name]
+	sensorSettings = iow.file2list( sets )
+
+	# here we are converting python object to json string
+	sensor_sets = json.loads( ' '.join(sensorSettings))
+	print json.dumps(sensor_sets, sort_keys=True, indent=2)
+	return sensor_sets
+	
+class SensorChannalHall():
+	_addac = None
+	_splitter_params = None
+	_sensor_curve_cb = None
+	_sensor_curve_sets = None
+	def __init__(self, sensor_sets, addacName, hxName, curve_cb ):
 		# Параметры делителя напряжения
-		splitter_params = sensor_sets['splitter_params']
-		R1 = splitter_params['R1']
-		R2 = splitter_params['R2']
-		splitter = R2/(R1+R2)
+		self._splitter_params = sensor_sets[hxName]
 
 		# Параметры ADDAC
-		addac = sensor_sets['addac']
-		dVmax = addac[ 'dVmax' ]	# mV сдвиг ЦАП
-		VmaxIdeal = addac[ 'VmaxIdeal' ]
+		self._addac = sensor_sets[ addacName ]
+		
+		# кривая сенсора
+		self._sensor_curve_cb = curve_cb
+		self._sensor_curve_sets = sensor_sets['sensor_curve_params']
 
-		capacity = addac[ 'capacity' ]
+	def getSplitter( self ):
+		R1 = self._splitter_params['R1']
+		R2 = self._splitter_params['R2']
+		splitter = R2/(R1+R2)
+		return splitter
+		
+	def toBitCoeff( self ):
+		dVmax = self._addac[ 'dVmax' ]	# mV сдвиг ЦАП
+		VmaxIdeal = self._addac[ 'VmaxIdeal' ]
+
+		capacity = self._addac[ 'capacity' ]
 		Vmax = VmaxIdeal-dVmax 	# mV - это максимальное значение ЦАП - площадка при выс. сигн.
 		resolution = math.pow(2, capacity)
 
 		toDigital = resolution/Vmax
-		toAnalog = 1/toDigital
+		return toDigital
 		
+	def toWaveCoeff( self ):
+		return 1/self.toBitCoeff( )
+		
+	def sensor_curve(self, value):
+		return self._sensor_curve_cb(value, self._sensor_curve_sets)
 		
 # Параметры входной кривой
 # Датчик Холла
-def value2voltageHall(value, sensor_sets):
-	curve_params = sensor_sets['curve_params']
+def value2voltageHall(value, curve_params):
 	Kiu = curve_params[ 'Kiu' ]		# mV/A
 	dU = curve_params[ 'dU' ]		# mV
-	U = I * Kiu + dU
+	U = value * Kiu + dU
 	return U
 	
 value2voltage = value2voltageHall
+SensorChannal = SensorChannalHall
 	
