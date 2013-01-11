b54f9ffc7891d46b2a02bbfd13565edc478777cf
unknown <igor@igor.ru>
2013-01-11 16:30:41 +0300

    push

--- models/shift_correction_model.py
+++ models/shift_correction_model.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python
 #-*- coding: utf-8 -*-
 import sys
-sys.path.append('D:/home/lugansky-igor/libs-dev/py-reuse-pkgs')
 import math
 
+# Other
+sys.path.append('D:/home/lugansky-igor/libs-dev/py-reuse-pkgs')
 import convertors_simple_data_types.IntTypeConvertors as tc
 import convertors_simple_data_types.Float32Convertors as f32_conv
 from py_dbg_toolkit.doColoredConsole import co
@@ -14,12 +14,11 @@ _nprint = co.printN
 _wprint = co.printW
 _eprint = co.printE
 
-''' '''
 def printFormatter(string):
     string = '0x'+string
     return string[:-1].replace(' ', ', 0x')
 
-''' 
+""" 
     Метод отображения результатов и плагины для вывода на комм. строку
     
     notes. : Низший модуль передает полностью всю информацию. Потом можно разбить
@@ -27,14 +26,14 @@ def printFormatter(string):
 
     rem. : функции обратного вызова можно собрать в кортеж и внизу управлять 
       действиями по имени
-'''
+"""
 # подборка плагинов
-def plotPlugin(string):    # пустой
+def plot_plugin(string):    # пустой
     None
-def plotPluginFull(string):
+def plot_plugin_full(string):
     print string
     
-_PLUGIN_LIST = {"None" : plotPlugin, 'Full':plotPluginFull}
+_PLUGIN_LIST = {"None" : plot_plugin, 'Full':plot_plugin_full}
 def _plot_item(msg, value):
     print msg+" "+str(value)
     ieee, mchip = f32_conv.run(value, _PLUGIN_LIST["None"])
@@ -44,8 +43,10 @@ def _plot_item(msg, value):
     lst.append(';\t'+msg+' '+mchip+'\n')
     iow.list2file(_SETS, lst)
 
-''' msg : Lhl Hhl'''
+
 def _plot_word(msg, word):
+    """ msg : Lhl Hhl"""
+    
     string = f32_conv.byte2hex(int(word)%256)     # L
     string += ' '+ f32_conv.byte2hex(int(word)/256)    # H
     print msg+' '+string
@@ -73,35 +74,34 @@ def _hex_word_to_int(hexWord):
         sum += oneIt
     return sum
 
-''' 
-    version : 1.0
-    
-    notes:
-        v 1.0
-          precond.:
-            1. попр. коэфф. всегда берется по модулю
-            2. при коррекции кода склад. или выч. в зависимости от знака коэфф. коррекции
-          contraints :
-            
-    math:
-        u_shift = u_shift_src+K*T    [float32]
-        u_shift_code = to_code*(from_code*u_shift_src_code+K*T) = 
-            u_shift_src+to_code*(K*T) = u_shift_src + int(T*(to_code*K)) = 
-            u_shift_src+sign(K)*int(T*(to_code*abs(K)))
-'''
 def calc_for_ukv()
+    """ 
+        @version : 1.0
+        
+        @notes:
+            v 1.0
+              precond.:
+                1. попр. коэфф. всегда берется по модулю
+                2. при коррекции кода склад. или выч. в зависимости от знака коэфф. коррекции
+              contraints :
+                
+        @math:
+            u_shift = u_shift_src+K*T    [float32]
+            u_shift_code = to_code*(from_code*u_shift_src_code+K*T) = 
+                u_shift_src+to_code*(K*T) = u_shift_src + int(T*(to_code*K)) = 
+                u_shift_src+sign(K)*int(T*(to_code*abs(K)))
+    """
     # Расчет для УКВ ЧМ
     T = 10    # Температура 8 бит бит/градус
     src_shift_code = '0111'    # значение кода для установки смещения по умолчанию из EEPROM
-
-    # температурный коэффициент
-    corrected_mult = -4.9 * 5 * 1e-3    # V/oC
-    corrected_mult = math.fabs(corrected_mult)    # ufloat
-
-    # поправка
     out_proportion = 4000 / 4.6    # ue/V число, загружаемое в ЦАП 
+    corrected_mult = -4.9 * 5 * 1e-3    # температурный коэффициент, V/oC
+
+    # Run
+    corrected_mult = math.fabs(corrected_mult)    # ufloat
+    # поправка
     mult = corrected_mult * out_proportion    # V/oC положительная!
-    dU = mult * T    # deltaU V uintXX
+    dU = mult * T    # dU V uintXX
 
     # значение изначального кода смещения для расчетов
     src_shift_code = _hex_word_to_int(src_shift_code)
@@ -115,9 +115,9 @@ def calc_for_ukv()
     _plot_item(msg, T)
     msg = 'mult, V/oC :'
     _plot_item(msg, mult)
-    msg = 'deltaU, ue LH:'
+    msg = 'dU, ue LH:'
     _plot_word(msg, dU)
-    msg = 'deltaU, ue :'
+    msg = 'dU, ue :'
     _plot_item(msg, dU)
     msg = 'Out shift value, ue LH:'
     _plot_word(msg, out_shift_code)
