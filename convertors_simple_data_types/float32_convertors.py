8fd3923cd4545e3e77487aea94834a7f4c3328e9
zaqwes <igor.a.lugansky@gmail.com>
2013-01-25 14:37:43 +0300

    Add code

---
+++ convertors_simple_data_types/float32_convertors.py
@@ -0,0 +1,189 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+# import strFloatToBitArrPresent
+
+import math as m
+import xintyy_type_convertors as xint_module
+
+ 
+def hexstr2float(src):
+    """ seee_eeee emmm_mmmm mmmm_mmmm mmmm_mmmm    - по стандарту """   
+    srcList = src.split(' ')
+    intList = list('')
+    for i in srcList:
+        for j in i:
+            intList.append(hex2int(j))
+            
+    # к обратному преобразованию готово
+    expExtr =  intList[2]+intList[1]*16+intList[0]*16*16
+    expExtr = expExtr >> 3
+    expExtr = expExtr & 255
+    # мантисса
+    mant = intList[-1]+intList[-2]*16+intList[-3]*16*16+\
+        intList[-4]*16*16*16+\
+        intList[-5]*16*16*16*16+(intList[-6]&7)*16*16*16*16*16
+    mant = float(mant)/m.pow(2, 23)+1  # полсностью в скобках
+    summaryo = mant*m.pow(2, (expExtr-127))
+    if intList[0] >= 9:
+        summaryo = -summaryo
+    return summaryo
+
+def hexMCHIPfloat32toFloat(src):
+    """ Функция преобразования float MICROCHIP 32 to IEEE float32
+        seee_eeee emmm_mmmm mmmm_mmmm mmmm_mmmm    - по стандарту
+        0    1    2    3    -4   -3   -2   -1
+        eeee_eeee smmm_mmmm mmmm_mmmm mmmm_mmmm - микрочиповский формат
+    """
+    srcList = src.split(' ')
+    intList = list('')
+    for i in srcList:
+        for j in i:
+            intList.append(xint_module.hex2int(j))
+            
+    # к обратному преобразованию готово
+    # выделяем экспаненту
+    expExtr =  intList[1]+intList[0]*16
+
+    # мантисса
+    mant = intList[-1]+\
+        intList[-2]*16+\
+        intList[-3]*256+\
+        intList[-4]*16*256+\
+        intList[-5]*256*256+\
+        (intList[-6]&7)*16*256*256
+    
+    mant = float(mant)/m.pow(2, 23)+1  # полсностью в скобках
+    result = mant*m.pow(2, (expExtr-127))
+    if intList[-6]&8 != 0:
+        result = -result
+    return result
+    
+def hex_mchip_float_to_float(src_string):
+    """ 0xXX 0xYY 0xRR 0xVV """
+    src_string = src_string.replace('0x','')
+    src_string = src_string.replace(',','')
+    return hexMCHIPfloat32toFloat(src_string)
+    
+    
+def float_to_hex32(float_value, plot_callback):
+    """ """
+    abs_in_value = abs(float_value)
+    # Точность предельная
+    count_steps = 23+1
+
+    # поиск подходящей степени
+    begin = 0
+    while 1:
+        power = m.pow(2, begin)
+        if abs_in_value-int(power) < 0:
+            begin -= 1     # нужно отнять
+            break
+    
+        # степень растет
+        begin += 1
+    
+    # степень отрицательная
+    if abs_in_value < 1:
+        begin = 0    # нолик всегда должен быть
+
+    # представляем в двoичном коде
+    res = 0
+    summary = ''
+    for i in range(count_steps):
+        # сохраняемся перед делением
+        res = abs_in_value/m.pow(2, begin-i)
+        
+        # принимае решение о бите
+        if res < 1 :
+            summary += '0'
+        else :
+            summary += '1'
+            abs_in_value = abs_in_value-int(res)*m.pow(2, begin-i)
+            
+        # ставим запятую
+        if m.pow(2, begin-i) == 1:
+            summary += ','
+            
+    # zero - ok
+    #print summary
+    
+    # Нужно узнать экспаненту, ищем где запятая и первая 1
+    first_one = summary.find('1')
+    coma = summary.find(',')
+    exp = 0
+    if first_one != -1: # единиц нет
+        if coma < first_one:
+            exp = coma-first_one
+        else :
+            exp = coma-first_one-1
+    else:
+        exp = -127
+    
+    # значение экспаненты
+    exp += 127    # задаем смещение 
+    
+    exp = xint_module.char2bitarray(exp)
+
+    # Итоговое формирование
+    # ставим знак числа
+    if str(float_value)[0] == '-' :
+        exp = '1'+exp
+    else :
+        exp = '0'+exp
+
+    # Офформление мантиссы
+    summary = summary.replace(',','')
+    position_firs_one = summary.find('1')
+    value_mant_in_str = summary[position_firs_one+1: -1]
+    len_value_mant_in_str = len(value_mant_in_str)
+    for j in range(23-len_value_mant_in_str):
+        value_mant_in_str += '0'
+
+    # Итого сцепляем и выводим
+    summary = exp+value_mant_in_str
+
+    # Форматируем
+    summary_tmp = '0000000'+summary+'0'
+    float32 = ''
+    for k in range(32+4+4):
+        float32 += summary_tmp[k]
+        if((k+1)%4 == 0):
+            float32 += ' '
+    aux_format_result = float32
+
+    float32=''
+    for k in range(32):
+        float32 += summary[k]
+        if((k+1)%4 == 0):
+            float32 += ' '
+    
+    # первый результат
+    ieee = float32
+
+    # преобразование числа для pic
+    pure_exp = aux_format_result.split(' ')
+    pure_exp = pure_exp[2]+' '+pure_exp[3]
+    sign = ieee[0][0]
+    
+    # разбиваем еще раз
+    mplab_float = ieee.split(' ')  # seee0_eeee1 e
+    mplab_format = pure_exp
+    tmp = '' 
+    for i in mplab_float[3:]:
+        tmp = tmp+' '+i
+        
+    # второй результат
+    mplab_format = mplab_format + ' '+sign+mplab_float[2][1:]+tmp
+    
+    # Проверка И вывод разультатов
+    if plot_callback:
+        plot_callback( "IEEE : "+str(float_value) + 
+            ' : '+xint_module.bit_formatted_array_to_hex(ieee)+' -> '+ieee)
+        plot_callback( "MICROCHIP : "+str(float_value) + 
+            ' : '+xint_module.bit_formatted_array_to_hex(mplab_format)+' -> '+mplab_format)
+     
+    return xint_module.bit_formatted_array_to_hex(ieee), xint_module.bit_formatted_array_to_hex(mplab_format)
+    
+#if name
+
+
--- models/shift_correction_model.py
+++ models/shift_correction_model.py
@@ -4,18 +4,19 @@ import math
 
 # Other
 import convertors_simple_data_types.xintyy_type_convertors as tc
-import convertors_simple_data_types.float32_convertors as f32_conv
+import convertors_simple_data_types.float32_convertors as f32conv
 from py_dbg_toolkit.doColoredConsole import co
 import uasio.os_io.io_wrapper as iow
 
-_SETS = { 'name': 'convertion.log', 'howOpen': 'a', 'coding': 'cp1251'}
+_kSets = { 'name': 'convertion.log', 'howOpen': 'a', 'coding': 'cp1251'}
+_kOutProportion = 4000/4.6    # ue/V число, загружаемое в ЦАП 
+
+# shortcuts
 _nprint = co.printN
 _wprint = co.printW
 _eprint = co.printE
 
-
-
-def printFormatter(string):
+def _print_formatter(string):
     string = '0x'+string
     return string[:-1].replace(' ', ', 0x')
 
@@ -38,12 +39,17 @@ _kPluginList = {"None" : plot_plugin, 'Full':plot_plugin_full}
 
 def _plot_item(msg, value):
     print msg+" "+str(value)
-    ieee, mchip = f32_conv.float_to_hex32(value, _kPluginList["None"])
-    mchip = printFormatter(mchip)
+    ieee, mchip = f32conv.float_to_hex32(value, _kPluginList["None"])
+    mchip = _print_formatter(mchip)
     
     lst = list()
-    lst.append(';\t'+msg+' '+mchip+'\n')
-    iow.list2file(_SETS, lst)
+    lst.append(';    '+msg+' '+mchip+'\n')
+    iow.list2file(_kSets, lst)
+    
+def _print_string(msg):
+    lst = list()
+    lst.append(';    '+msg+'\n')
+    iow.list2file(_kSets, lst)
 
 
 def _plot_word(msg, word):
@@ -54,13 +60,13 @@ def _plot_word(msg, word):
     print msg+' '+string
 
     lst = list()
-    lst.append(';   '+msg+' '+string+'\n')
-    iow.list2file(_SETS, lst)
+    lst.append(';    '+msg+' '+string+'\n')
+    iow.list2file(_kSets, lst)
     
 def _new_line():
     lst = list()
     lst.append('\n')
-    iow.list2file(_SETS, lst)
+    iow.list2file(_kSets, lst)
 
 def _eprint_value(name, value):
     _eprint(name+' : '+str(value)+'\n')
@@ -76,8 +82,12 @@ def _hex_word_to_int(hexWord):
         sum += oneIt
     return sum
 
-def calc_for_ukv():
-    """ 
+def calc_for_ukv(
+        correcting_mult,  # температурный коэффициент, V/oC
+        T,  # Температура 8 бит бит/градус
+        src_shift_code  # значение кода для установки смещения по умолчанию из EEPROM
+        ):
+    """ Расчет для УКВ ЧМ
         @version : 1.0
         
         @notes:
@@ -93,30 +103,28 @@ def calc_for_ukv():
                 u_shift_src+to_code*(K*T) = u_shift_src + int(T*(to_code*K)) = 
                 u_shift_src+sign(K)*int(T*(to_code*abs(K)))
     """
-    # Расчет для УКВ ЧМ
-    T = 10    # Температура 8 бит бит/градус
-    src_shift_code = '0111'    # значение кода для установки смещения по умолчанию из EEPROM
-    out_proportion = 4000 / 4.6    # ue/V число, загружаемое в ЦАП 
-    corrected_mult = -4.9 * 5 * 1e-3    # температурный коэффициент, V/oC
 
     # Run
-    corrected_mult = math.fabs(corrected_mult)    # ufloat
+    correcting_mult = math.fabs(correcting_mult)    # ufloat
     # поправка
-    mult = corrected_mult * out_proportion    # V/oC положительная!
-    dU = mult * T    # dU V uintXX
+    K = correcting_mult*_kOutProportion    # ue(uint16)/oC положительная!
+    dU = K*T    # dU ue(uint16)
 
     # значение изначального кода смещения для расчетов
     src_shift_code = _hex_word_to_int(src_shift_code)
 
     # uintXX = uintXX+(or -)uintXX
-    out_shift_code = src_shift_code+math.copysign(1, corrected_mult)*dU    # вычитание вот здесь!
-
+    out_shift_code = src_shift_code+math.copysign(1, correcting_mult)*dU    # вычитание вот здесь!
+    
     # Report
     msg = 'T oC :'
     _plot_word(msg, T)
     _plot_item(msg, T)
-    msg = 'mult, V/oC :'
-    _plot_item(msg, mult)
+    
+    msg = 'K, ue(uint16)/oC:'
+    _print_string(msg+' '+str(K))
+    _plot_item(msg, K)
+    
     msg = 'dU, ue LH:'
     _plot_word(msg, dU)
     msg = 'dU, ue :'
