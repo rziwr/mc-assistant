e780078d182037d5d2433ac11ac220216b56eafc
zaqwes <igor.a.lugansky@gmail.com>
2014-02-17 09:16:00 +0300

    save and push

--- convertors_simple_data_types/test_float32_convertors.py
+++ convertors_simple_data_types/test_float32_convertors.py
@@ -6,95 +6,102 @@
 import float32_convertors as f32cnv
 import unittest
 
-def plotCallBack( string ):
+
+def plotCallBack(string):
     pass
-    
+
+
 class TestFloatConversion(unittest.TestCase):
     def setUp(self):
-        pass 
-        
+        pass
+
     ''' '''
-    def testInv( self ):
+
+    def testInv(self):
         # проверим обр. преобр
         doubleOne = 1.0
         doubleOneFromMCHIP = f32cnv.hexMCHIPfloat32toFloat("7F 00 00 00")
-        
+
         self.assertEqual(doubleOneFromMCHIP, doubleOne)
-        
+
         # проверим обр. преобр
         doubleOne = 0.5
         doubleOneFromMCHIP = f32cnv.hexMCHIPfloat32toFloat("7E 00 00 00")
-        
+
         self.assertEqual(doubleOneFromMCHIP, doubleOne)
-        
-    def testInvIEEEOneValue( self ):
+
+    def testInvIEEEOneValue(self):
         self.assertEqual(f32cnv.hexstr2float("3F 80 00 00"), 1.0)
-        
-    def testInvMCHIPOneValue( self ):
+
+    def testInvMCHIPOneValue(self):
         self.assertEqual(f32cnv.hexMCHIPfloat32toFloat("7F 00 00 00"), 1.0)
-        
-    def testInvIEEETwoValue( self ):
+
+    def testInvIEEETwoValue(self):
         self.assertEqual(f32cnv.hexstr2float("40 00 00 00"), 2.0)
-        
-    def testInvMCHIPTwoValue( self ):
+
+    def testInvMCHIPTwoValue(self):
         self.assertEqual(f32cnv.hexMCHIPfloat32toFloat("80 00 00 00"), 2.0)
-        
-    def testIEEEOneValue( self ):
+
+    def testIEEEOneValue(self):
         doubleOne = 1.0
         # проверка преобразования
-        a, doubleOneDirectCnvMCHIP = f32cnv.float_to_hex32(doubleOne, plotCallBack)
+        m, a, doubleOneDirectCnvMCHIP = f32cnv.float_to_hex32(doubleOne, plotCallBack)
         self.assertEqual(f32cnv.hexstr2float(a), doubleOne)
-        
-    def testIEEETwoValue( self ):
+
+    def testIEEETwoValue(self):
         doubleOne = 2.0
         # проверка преобразования
-        a, doubleOneDirectCnvMCHIP = f32cnv.float_to_hex32(doubleOne, plotCallBack)
+        m, a, doubleOneDirectCnvMCHIP = f32cnv.float_to_hex32(doubleOne, plotCallBack)
         self.assertEqual(f32cnv.hexstr2float(a), doubleOne)
-    
-    def testIEEEHalfValue( self ):
+
+    def testIEEEHalfValue(self):
         doubleOne = 0.5
         # проверка преобразования
-        a, doubleOneDirectCnvMCHIP = f32cnv.float_to_hex32(doubleOne, plotCallBack)
+        m, a, doubleOneDirectCnvMCHIP = f32cnv.float_to_hex32(doubleOne, plotCallBack)
         self.assertEqual(f32cnv.hexstr2float(a), doubleOne)
-    
-    ''' преобразование 1 и 2 
+
+    ''' преобразование 1 и 2
         ошибка 1 = 0.5
-    ''' 
-    def testOneValue( self ):
+    '''
+
+    def testOneValue(self):
         doubleOne = 1.0
         # проверка преобразования
-        a, doubleOneDirectCnvMCHIP = f32cnv.float_to_hex32(doubleOne, plotCallBack)
+        m, a, doubleOneDirectCnvMCHIP = f32cnv.float_to_hex32(doubleOne, plotCallBack)
         self.assertEqual(f32cnv.hexMCHIPfloat32toFloat(doubleOneDirectCnvMCHIP), doubleOne)
-        
+
     ''' '''
-    def testTwoValue( self ):
+
+    def testTwoValue(self):
         doubleOne = 2.0
         # проверка преобразования
-        a, doubleOneDirectCnvMCHIP = f32cnv.float_to_hex32(doubleOne, plotCallBack)
+        m, a, doubleOneDirectCnvMCHIP = f32cnv.float_to_hex32(doubleOne, plotCallBack)
         self.assertEqual(f32cnv.hexMCHIPfloat32toFloat(doubleOneDirectCnvMCHIP), doubleOne)
-        
+
     ''' '''
-    def testHalfValue( self ):
+
+    def testHalfValue(self):
         doubleOne = 0.5
         # проверка преобразования
-        a, doubleOneDirectCnvMCHIP = f32cnv.float_to_hex32(doubleOne, plotCallBack)
+        message, a, doubleOneDirectCnvMCHIP = f32cnv.float_to_hex32(doubleOne, plotCallBack)
         self.assertEqual(f32cnv.hexMCHIPfloat32toFloat(doubleOneDirectCnvMCHIP), doubleOne)
-        
+
     ''' Просто тест на работоспособность '''
-    def testSimple( self ):
+
+    def testSimple(self):
         # IEEE
-        self.assertEqual( f32cnv.hexstr2float("43 1B A0 00"), 155.625 )
-        
+        self.assertEqual(f32cnv.hexstr2float("43 1B A0 00"), 155.625)
+
         # MCHIP
-        self.assertEqual( f32cnv.hexstr2float("43 1B A0 00"), 155.625 )
-        
+        self.assertEqual(f32cnv.hexstr2float("43 1B A0 00"), 155.625)
+
     def testZero(self):
         doubleOne = 0.0
         # проверка преобразования
-        a, doubleOneDirectCnvMCHIP = f32cnv.float_to_hex32(doubleOne, None)
+        m, a, doubleOneDirectCnvMCHIP = f32cnv.float_to_hex32(doubleOne, None)
         self.assertEqual(a[:-1], '00 00 00 00')
-        
+
 # Run tests
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase( TestFloatConversion )
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestFloatConversion)
     unittest.TextTestRunner(verbosity=2).run(suite)
