f3b83d109239e5093dd71fffbfc9b2bbb9023116
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-12-01 15:33:43 +0300

    add C++ cmake project

---
+++ embedded/cc-cmake-test-build/CMakeFiles/cc-cmake-test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cc-cmake-test.dir/main.cpp.obj"
+  "cc-cmake-test.pdb"
+  "cc-cmake-test.exe"
+  "cc-cmake-test.exe.manifest"
+  "libcc-cmake-test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cc-cmake-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ embedded/cc-cmake-test-build/CMakeFiles/cc-cmake-test.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cc-cmake-test.
+# This may be replaced when dependencies are built.
