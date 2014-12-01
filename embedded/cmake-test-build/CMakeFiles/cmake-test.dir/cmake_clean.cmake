b9ac5ecbe8798abd1ac9e140d7e597dff38d9573
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-12-01 15:31:01 +0300

    first C cmake project

---
+++ embedded/cmake-test-build/CMakeFiles/cmake-test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake-test.dir/main.c.obj"
+  "cmake-test.pdb"
+  "cmake-test.exe"
+  "cmake-test.exe.manifest"
+  "libcmake-test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmake-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
